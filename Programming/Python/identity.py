e99f5d4dea99ea4b2f5b33732f7a2d69c6a7b49b
Sai <saibillas@gmail.com>
2018-12-28 18:54:49 +0530

    py

---
+++ Programming/Python/identity.py
@@ -0,0 +1,7 @@
+w="varun"
+v="varun"
+c=w
+print(w is v)
+print(v is c)
+print(w is not c)
+print(w is c)
---
+++ Programming/Python/kjfjdfvfdhjv d.py
@@ -0,0 +1,4 @@
+sting1="                                            hello "
+string2=" world                                              "
+cus=len()
+print(cus)
