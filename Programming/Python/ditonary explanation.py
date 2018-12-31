7c0ddef004fe2a6e0276dfb8204b4ec3137b891d
Sai <saibillas@gmail.com>
2018-12-31 17:06:14 +0530

    py

---
+++ Programming/Python/ditonary explanation.py
@@ -0,0 +1,7 @@
+print("hi guys today we are going to learn about dictonaries")
+print("so today im not going to give explaination so i will show two examples")
+mydic={1:"green" , 2:"blue"}
+    #if your writing a long program and forgot some of the keys this will help you and second one will show the values
+print(mydic.keys())
+print(mydic.values()) 
+																			
