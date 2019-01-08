954f60598d872848a6f62692edf33f72bc18e5e4
Sai <saibillas@gmail.com>
2019-01-08 19:33:33 +0530

    py

---
+++ Programming/Python/forloop.py
@@ -0,0 +1,10 @@
+for python in range(70,0,-1):
+	if python >1 :
+		print(python,"may god bless you" )
+		if python < 60:
+			sufix= str(python)+ "varun"
+		else:
+			sufix = " happy birthday"
+			                             
+ 
+			
