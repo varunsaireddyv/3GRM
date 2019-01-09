8b4ee82b018f12bbbbe3be0b912deb4699893b67
Sai <saibillas@gmail.com>
2019-01-09 18:02:55 +0530

    py

---
+++ Programming/Python/continue.py
@@ -0,0 +1,4 @@
+for v in range(20):
+	if (v%2)==0:
+		continue
+        print(v)
---
+++ Programming/Python/functions.py
@@ -0,0 +1,14 @@
+def sub (a, b) :
+	result = a - b
+	return result
+print("for subtraction")	
+num1=input ("num1=")
+num2=input ("num2=")
+print(sub(num2, num1))
+def add (c, d) :
+	result = c + d
+	return result
+print("for addition")
+num3=input ("num3=")
+num4=input ("num4=")
+print(add(num3, num4))
