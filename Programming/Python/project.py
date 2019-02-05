c51a19893fdb598139eb98f1a47e318be1304ffb
Sai <saibillas@gmail.com>
2019-02-05 20:01:58 +0530

    py

--- Programming/Python/project.py
+++ Programming/Python/project.py
@@ -6,7 +6,7 @@ import webbrowser
 
 def talktome(audio):
 	print(audio)
-	tts=gTTs(Text=audio. lang = 'en')
+	tts=gTTs(Text=audio. lang='en')
 	tts.save('audio.mp3')
 	os.system('mpg123 audio.mp3') 
 	
@@ -14,10 +14,39 @@ def talktome(audio):
 
 def mycommand():
 	
-	r=sr.reconiser()
+	r=sr.reconizer()
 	
 	with sr.microphone() as sourse:
 		print('I am, ready for your next command')
 		r.pause_threshhold = 1
 		r.adjust_for_ambient_noise(sorce.doration=1)
-		audio=r.listening
+		audio=r.listen(source)
+		
+		try:
+			command=r.recognize_google(audio)
+			print('You said' + command)= '/n'
+			
+#loop back
+
+except sr.UnknownValueError:
+	assitant(mycommand())
+	
+	return command
+	
+#if staments
+def assistant(command):
+	
+	if 'open email'in command:
+		chrome_path='/user/bin/google-chrome'
+		url='https://www.email.com/r/python'
+		webbrowser.get(chrome_path).open(url)
+		
+		if 'What\'s upp' im command:
+			talktome('Chilln bro')
+		if 'email' in command:
+			talktome('Who is the recipiant')
+			recipient=mycommand()
+			
+			if 'rajesh ,bhumi ,varun' in recipien:
+				talktome('What should I say')
+				content=mycommand()
