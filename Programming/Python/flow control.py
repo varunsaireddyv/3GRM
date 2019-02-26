a7852156c3d9e4e513a1a16129a33f8187e2a305
Sai <saibillas@gmail.com>
2019-02-26 16:48:20 +0530

    push

--- Programming/Python/flow control.py
+++
@@ -1,7 +0,0 @@
-mymarks=80
-if(mymarks <=80):
-   print("gradeA")
-elif(mymarks <60)and (mymarks >=100):
-	print("grade B")
-else:
-	print"fail"
---
+++ Programming/Python/grades.py
@@ -0,0 +1,9 @@
+mymarks=input("enter your marks = ")
+if(mymarks ==100):
+   print("you have got out of out in your exam/s congratulatoins")
+elif(mymarks >=80):
+	print("you have got A grade in your exam/s congratulations")
+elif(mymarks <=80):
+	print("you have got B grade not bad")
+else:
+	print("you have failled you need improvment")
