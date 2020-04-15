8852cf1496c26f8d3f666f40b7273d230293e940
unknown <varunbillas@gmail.com>
2020-04-15 17:57:35 +0530

    my test even numbers if else

---
+++ Programming/Python/samplescripts/IfElse.py
@@ -0,0 +1,7 @@
+x = 0
+while x < 10:
+    if x == 5:
+        x = x+1
+        continue
+    print(x)
+    x = x+1
---
+++ Programming/Python/samplescripts/evennumbers.py
@@ -0,0 +1,7 @@
+x=0
+while True :
+    if x < 10 :
+         x = x+2
+         print(x)
+    else :
+        break
