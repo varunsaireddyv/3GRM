ce4b89168aec481fd73c218953182783c80ee3f3
unknown <varunbillas@gmail.com>
2020-04-22 12:07:07 +0530

    many things

---
+++ Programming/Python/samplescripts/a.py
@@ -0,0 +1,4 @@
+list = ['list can hold strings and numbers', 1, 2, 3]
+print (list)
+tup = ('tuples are immutable', 'scenario 1' )
+tup.add(2009)
---
+++ Programming/Python/samplescripts/learning2.py
@@ -0,0 +1,4 @@
+mylist = [10,20,30,40,20,30, 'varun sai']
+
+mylist.append('varunsai')
+print(mylist)
