eb395599d8aa88e9fc37ab7612ce72574fe44e6d
RajeshReddyG <gundupallirajesh@gmail.com>
2020-10-27 15:15:29 +0530

    turtle package

--- Programming/Python/diet/diet.py
+++ Programming/Python/diet/diet.py
@@ -1,7 +1,7 @@
 import pandas
-#import os
-# print(os.curdir)
-# print("please select the item id:\n")
+import os
+ print(os.curdir)
+ print("please select the item id:\n")
 df = pandas.read_csv("/home/sai/Desktop/FoodDataSet.csv", usecols = ['Menu_Item_ID','Item_Name','Food_Category'])
 print(df)
-# a = input('enter your numbers')
+a = input('enter your numbers')
