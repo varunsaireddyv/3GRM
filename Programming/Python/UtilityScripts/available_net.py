87d260a62c5a1c46de57e1c71cc27a6825325ef9
RajeshReddyG <gundupallirajesh@gmail.com>
2020-11-16 14:40:11 +0530

    test for available internet, gui fo app, jarvis, turtle package, alphabets from turtle, fibo sieries

---
+++ Programming/Python/UtilityScripts/available_net.py
@@ -0,0 +1,8 @@
+import subprocess 
+  
+devices = subprocess.check_output(['netsh','wlan','show','network']) 
+
+devices = devices.decode('ascii') 
+devices= devices.replace("\r","") 
+
+print(devices)
